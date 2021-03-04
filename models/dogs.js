84fd622c777c6dde54fe8670deb3768d74c09050
Lewis Stokes <stokesl4@coventry.ac.uk>
2021-03-04 02:54:35 +0000

    Added routing for users and updated model functions to arrow functions

--- models/dogs.js
+++ models/dogs.js
@@ -6,7 +6,7 @@ const db = require('../helpers/database');
 /* - - - - Creation - - - - */
 
 // Register new dog on system
-exports.newDog = async function newDog(dog) {
+exports.newDog = async (dog) => {
 	const query = "INSERT INTO dogs SET ?";
 
 	let result = await db.exec(query, dog);
@@ -18,7 +18,7 @@ exports.newDog = async function newDog(dog) {
 /* - - - - Retrieval - - - - */
 
 // Returns all dogs in database
-exports.getAll = async function getAll() {
+exports.getAll = async () => {
 	const query = "SELECT * FROM dogs;"
 
 	let result = await db.exec(query);
@@ -28,7 +28,7 @@ exports.getAll = async function getAll() {
 
 
 // Returns all dog records at specific site
-exports.getByStoreId = async function getByStoreId(id) {
+exports.getByStoreId = async (id) => {
 	const query = "SELECT * FROM dogs WHERE SiteID = ?;";
 	const data = [id];
 
@@ -39,7 +39,7 @@ exports.getByStoreId = async function getByStoreId(id) {
 
 
 // Returns all dog records listed as specified breed
-exports.getByBreed = async function getByBreed(breed) {
+exports.getByBreed = async (breed) => {
 	const query = "SELECT * FROM dogs WHERE Breed = ?;";
 	const data = [breed];
 
@@ -50,7 +50,7 @@ exports.getByBreed = async function getByBreed(breed) {
 
 
 // Returns dog record specified by ID
-exports.getById = async function getById(id) {
+exports.getById = async (id) => {
 	const query = "SELECT * FROM dogs WHERE ID = ?;";
 	const data = [id];
 
