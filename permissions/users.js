d134d79c7fad6e585abcadd8bd36f46dede2f93a
Lewis Stokes <stokesl4@coventry.ac.uk>
2021-07-04 17:07:26 +0000

    Updated user permissions to include update and delete operations

--- permissions/users.js
+++ permissions/users.js
@@ -13,13 +13,14 @@ const ac = new AccessControl();
 ac.grant('user')
 	.condition({Fn:'EQUALS', args: {'requester':'$.owner'}}).execute('read').on('user', ['*', '!password'])
 	.condition({Fn:'EQUALS', args: {'requester':'$.owner'}}).execute('update').on('user', ['password', 'lastOnline', 'email', 'profileImg', 'firstName', 'lastName'])
+	.condition({Fn:'EQUALS', args: {'requester':'$.owner'}}).execute('delete').on('user', ['*'])
+
 
 ac.grant('admin')
-  	.execute('read').on('user');
-
-ac.grant('admin')
-	.execute('read').on('all_users');
-//	.execute('update').on('all_users');
+  	.execute('read').on('user')
+	.execute('read').on('all_users')
+	.execute('update').on('user')
+	.execute('delete').on('user');
 
 
 
@@ -39,16 +40,40 @@ exports.readAll = (req) => {
 
 /**
  * read
- * @description Checks whether user has permission to perform read operation on user
+ * @description Checks whether user has permission to perform read operation on user data
  *
  * @param {object} req - User object of requester
  * @param {object} data - User object of data to be read
  * @returns {boolean} User has sufficient permissions to complete action
  */
 exports.read = (req, data) => {
-
-
 	return ac.can(req.roles[0]).context({requester:req.ID, owner:data.ID}).execute('read').sync().on('user');
+};
+
+
+/**
+ * update
+ * @description Checks whether user has permission to perform update operation on user data
+ *
+ * @param {object} req - User object of requester
+ * @param {object} data - User object of data to be updated
+ * @returns {boolean} User has sufficient permissions to complete action
+ */
+exports.update = (req, data) => {
+	return ac.can(req.roles[0]).context({requester:req.ID, owner:data.ID}).execute('update').sync().on('user');
+};
+
+
+/**
+ * deleteUser
+ * @description Checks whether user has permission to perform delete operation on user data
+ *
+ * @param {object} req - User object of requester
+ * @param {object} data - User object to delete
+ * @returns {boolean} User has sufficient permissions to delete account
+ */
+exports.deleteUser = (req, data) => {
+	return ac.can(req.roles[0]).context({requester: req.ID, owner:data.ID}).execute('delete').sync().on('user');
 };
 
 //exports.read = (req, data) => ac.can(req.role).context
